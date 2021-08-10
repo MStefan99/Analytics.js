fa68509fa77ed74f0a6582135a00269b46600177
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-08-10 15:12:02 +0300

    added session graph

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,7 @@
 'use strict';
 
-
 import Jui from '/js/jui.js';
+
 
 (() => {
 	function svgElement(tag) {
@@ -9,24 +9,54 @@ import Jui from '/js/jui.js';
 	}
 
 
-	const websiteID = 'Analytics';
+	function maxUsers(sessions) {
+		const users = [];
 
+		for (const key of Object.keys(sessions)) {
+			users.push(sessions[key]);
+		}
+
+		return Math.max(...users);
+	}
+
+
+	const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+
+
+	const sessionLength = 60 * 1000;
+	const websiteID = 'Analytics';
 	const scriptLocation = new URL(import.meta.url);
 	const analyticsLocation = scriptLocation.host;
 
 	fetch('//' + analyticsLocation + '/api/stats/realtime/' + websiteID, {})
 		.then(res => res.json())
 		.then(json => {
+			console.log(json);
+
 			const activeUsersElement = document.getElementById('active-users');
 			activeUsersElement.innerText = json.currentUsers;
 
 			const realtimeSvg = new Jui('#active-users-timeline-svg');
+			const svgRect = realtimeSvg.nodes[0].viewBox.baseVal;
 
-			new Jui(svgElement('rect'))
-				.attr('x', 0)
-				.attr('y', 0)
-				.attr('width', 20)
-				.attr('height', 20)
-				.appendTo(realtimeSvg);
+			const max = maxUsers(json.sessions);
+			const startTime = Date.now() - (Date.now() % sessionLength) - sessionLength * 30;
+
+			for (let i = 0; i < 30; ++i) {
+				let users = 0;
+				if (json.sessions[startTime + sessionLength * i]) {
+					users = json.sessions[startTime + sessionLength * i];
+				}
+				console.log(svgRect);
+
+				const height = map(users, 0, max, 0, 100);
+				new Jui(svgElement('rect'))
+					.addClass('sessions-bar')
+					.attr('x', i * 7)
+					.attr('y', svgRect.height - height)
+					.attr('width', 6)
+					.attr('height', height)
+					.appendTo(realtimeSvg);
+			}
 		});
 })();
