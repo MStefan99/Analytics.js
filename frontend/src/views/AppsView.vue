30e2ae0beb0ca46e830ad5fe048fec2bb7d8ada2
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-03-11 23:20:15 +0200

    fixed app creation

--- frontend/src/views/AppsView.vue
+++ frontend/src/views/AppsView.vue
@@ -10,26 +10,26 @@
 			p {{app.description}}
 		.d-flex
 			button.mt-4(@click="newApp = emptyApp") Add an app
-		Transition(name="popup")
-			.popup-wrapper(v-if="newApp" @click.self="newApp = null")
-				.popup
-					h2.popup-title New app
-					form.popup-content(@submit.prevent="addApp")
-						.mb-3
-							label(for="name-input") Name
-							input#name-input.w-full(type="text" placeholder="Name" v-model="newApp.name")
-						.mb-3
-							label(for="description-input") Description
-							input#description-input.w-full(
-								type="text"
-								placeholder="Description"
-								v-model="newApp.description")
-						.mb-3
-							button.w-full(type="submit") Add app
 	div(v-else)
 		h2 No apps yet
 		p Add an app to start
-		button.mt-4 Add an app
+		button.mt-4(@click="newApp = emptyApp") Add an app
+	Transition(name="popup")
+		.popup-wrapper(v-if="newApp" @click.self="newApp = null")
+			.popup
+				h2.popup-title New app
+				form.popup-content(@submit.prevent="addApp")
+					.mb-3
+						label(for="name-input") Name
+						input#name-input.w-full(type="text" placeholder="Name" v-model="newApp.name")
+					.mb-3
+						label(for="description-input") Description
+						input#description-input.w-full(
+							type="text"
+							placeholder="Description"
+							v-model="newApp.description")
+					.mb-3
+						button.w-full(type="submit") Add an app
 </template>
 
 <script setup lang="ts">
