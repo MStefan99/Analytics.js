224b561c4549711e2e383695a4d39418c4a42982
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-12-19 21:36:44 +0200

    improved ua parsing

--- frontend/src/scripts/util.ts
+++ frontend/src/scripts/util.ts
@@ -1,17 +1,11 @@
-export function parseUA(ua: string): string | null {
-	const res = ua.match(/.*? \((.*?); (.*?)([;)]).*/);
-	let os: string | null;
+import bowser from 'bowser';
 
-	if (!res) os = ua ?? null;
-	else if (res[1] === 'Linux') os = res[2] ?? null;
-	else if (res[2] === 'Win64') os = res[1]?.replace('NT ', '')?.replace('.0', '') ?? null;
-	else if (res[1] === 'Macintosh')
-		os = 'macOS ' + res[2]?.replace(/.*Mac OS X (.*?)$/, '$1')?.replace(/_/g, '.') ?? null;
-	else if (res[1] === 'iPhone')
-		os = 'iPhone (iOS ' + res[2]?.replace(/.*OS (.*?) like.*/, '$1)')?.replace(/_/g, '.') ?? null;
-	else if (res[1] === 'iPad')
-		os = 'iPad (iPadOS ' + res[2]?.replace(/.*OS (.*?) like.*/, '$1)')?.replace(/_/g, '.') ?? null;
-	else os = res[1] ?? null;
+export function parseUA(ua: string) {
+	const parsed = bowser.parse(ua);
+	let str = `${parsed.browser.name} ${parsed.browser.version}`;
 
-	return os;
+	if (parsed.os?.name?.length) {
+		str += ` on ${parsed.os.name} ${parsed.os.version ?? ''}`;
+	}
+	return str;
 }
